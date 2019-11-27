40e8f2ef5b0aa116c304c1374262028b4d2eb38e
dennis_nijenhuis <dennis.nijenhuis@werum.com>
2019-11-27 09:24:38 +0100

    Erste version zum loggen der args

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace fw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+            if (args.Length > 0) {
+                Console.WriteLine("Alarm empfangen");
+                string fileName = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff") + "_" + Process.GetCurrentProcess().Id +"_fe2_alarm.txt";             
+
+                Directory.CreateDirectory("logs");
+
+                using (StreamWriter outputFile = new StreamWriter("./logs/" + fileName))
+                {
+                    int i = 0;
+                    foreach (var arg in args) {
+                        outputFile.WriteLine("arg" + i++ +": " + arg.ToString());
+                    }
+
+                }
+            }
+            else {
+                Console.WriteLine("Alarm nicht verfügbar");
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,11 @@
+# H1 AlarmLogger
+
+Testversion schreibt alle empfangenen Argumente in eine Textdatei mit Zeitstempel und PID.
+
+
+Entwicklungsumgebung:
+.Net Core SDK V3.0
+https://dotnet.microsoft.com/download
+
+Erzeugen eines self-contained Releases:
+dotnet publish -c Release -r win81-x64 --self-contained true
